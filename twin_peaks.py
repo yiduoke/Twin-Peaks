bf67f5271d07e9d66df81b6b1de7b6d727cadb8d
yiduoke <yiduoke822@jhs185.org>
2025-06-05 01:57:15 -0500

    changed port stuff

--- twin_peaks.py
+++ twin_peaks.py
@@ -45,6 +45,5 @@ def index():
     return render_template_string(HTML_TEMPLATE, result=result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-# To use the welcome.jpg image, place it in a folder named 'static' in the same directory as this script.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
